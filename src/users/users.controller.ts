44fd31741f8a961fc3f28b3d55ca66fa0978c829
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-08-03 14:24:41 +0200

    Creating error exception settings and http-error class protection

---
+++ src/users/users.controller.ts
@@ -0,0 +1,31 @@
+import {BaseController} from "../common/base.controller";
+import {LoggerService} from "../logger/logger.service";
+import {NextFunction, Request, Response} from "express";
+import {HTTPError} from "../errors/http-error.class";
+
+export class UsersController extends BaseController{
+    constructor(logger: LoggerService) {
+        super(logger);
+        this.bindRoutes([
+            {
+                path: '/register',
+                method: 'post',
+                func: this.register
+            },
+            {
+                path: '/login',
+                method: 'post',
+                func: this.login
+            },
+        ])
+    }
+
+    login(req: Request, res: Response, next: NextFunction){
+        this.ok(res,'login');
+        //next(new HTTPError(401,'error of authorization', 'login'));
+    }
+
+    register(req: Request, res: Response, next: NextFunction){
+        this.ok(res,'register');
+    }
+}
