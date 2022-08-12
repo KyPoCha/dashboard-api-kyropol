b088eac819942c987e41185bddc0991fbff288cd
Kyrylo Polishchuk <poliskyr@fit.cvut.cz>
2022-08-12 21:01:41 +0200

    Update main app files and package for e2e tests

--- src/app.ts
+++ src/app.ts
@@ -60,4 +60,8 @@ export class App {
 		this.server = this.app.listen(this.port);
 		this.logger.log(`Server is loaded on http://localhost:${this.port}`);
 	}
+
+	public close(): void {
+		this.server.close();
+	}
 }
